0e18dac43a530a1cb53bfc3dbc4fd6a5766ec7e6
Jake <zamorejake@gmail.com>
2023-08-14 12:16:51 -0500

    added about me and working on portfolio

--- src/assets/components/AboutMe.jsx
+++ src/assets/components/AboutMe.jsx
@@ -1,10 +1,20 @@
 import Header from './Header';
 const AboutMe = () => {
     return (
-      <body className="h-screen">
-        <Header about="bg-gray-500 " />
-        <p>about</p>
-      </body>
+      <>
+      <Header about="bg-gray-500" />
+      <body className="h-screen flex flex-col px-1.5 items-center">
+        <h1 className="text-gray-400 text-bold font-bold text-5xl pt-12">About Me</h1>
+        <div className="flex-shrink-0 pt-5">
+        <img src="pfp.png" alt="Profile Picture" width="200" height="200"/>
+        </div>
+        <p className="pt-12 px-14 text-3xl">Welcome to my corner of the web! I'm Jake Zam, a passionate MERN fullstack developer on a journey to create innovative and immersive digital experiences. With a strong foundation in MongoDB, Express, React, and Node.js, I thrive in crafting dynamic and responsive web applications that not only meet, but exceed expectations.
+        I've been fascinated with technology at an early age and would do my best to mess around with it to see how it all worked.
+        That drive has stuck with me and since then I've built many computers, set up my own server for home use, and deployed my own home network with multiple access points and VLAN segmenting.
+        Since then I have really been emersed in web development and plan to continue and grow as new technologies arrive!
+        </p>
+     </body>
+      </>
     );
   };
   
--- src/assets/components/Portfolio.jsx
+++ src/assets/components/Portfolio.jsx
@@ -1,10 +1,20 @@
 import Header from './Header';
 const Portfolio = () => {
   return (
-    <body className="h-screen">
-      <Header port="bg-gray-500" />
-      <p>testport</p>
+    <>
+    <Header port="bg-gray-500" />
+    <body className="h-screen flex flex-col px-1.5 items-center ">
+      <h1 className="text-gray-400 text-bold font-bold text-5xl pt-12">Portfolio</h1>
+      <div className="grid grid-cols-3 pt-12 gap-20">
+      <img src="pfp.png" alt="Profile Picture" width="200" height="200"/>
+      <img src="pfp.png" alt="Profile Picture" width="200" height="200"/>
+      <img src="pfp.png" alt="Profile Picture" width="200" height="200"/>
+      <img src="pfp.png" alt="Profile Picture" width="200" height="200"/>
+      <img src="pfp.png" alt="Profile Picture" width="200" height="200"/>
+      <img src="pfp.png" alt="Profile Picture" width="200" height="200"/>
+      </div>
     </body>
+    </>
   );
 };
 
