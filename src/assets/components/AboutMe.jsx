4491a57066e40bdf607434124ad37484e4545dde
Jake <zamorejake@gmail.com>
2023-08-14 13:02:42 -0500

    added logic for portfolio hover

--- src/assets/components/AboutMe.jsx
+++ src/assets/components/AboutMe.jsx
@@ -8,7 +8,7 @@ const AboutMe = () => {
         <div className="flex-shrink-0 pt-5">
         <img src="pfp.png" alt="Profile Picture" width="200" height="200"/>
         </div>
-        <p className="pt-12 px-14 text-3xl">Welcome to my corner of the web! I'm Jake Zam, a passionate MERN fullstack developer on a journey to create innovative and immersive digital experiences. With a strong foundation in MongoDB, Express, React, and Node.js, I thrive in crafting dynamic and responsive web applications that not only meet, but exceed expectations.
+        <p className="pt-12 px-14 text-3xl text-white">Welcome to my corner of the web! I'm Jake Zam, a passionate MERN fullstack developer on a journey to create innovative and immersive digital experiences. With a strong foundation in MongoDB, Express, React, and Node.js, I thrive in crafting dynamic and responsive web applications that not only meet, but exceed expectations.
         I've been fascinated with technology at an early age and would do my best to mess around with it to see how it all worked.
         That drive has stuck with me and since then I've built many computers, set up my own server for home use, and deployed my own home network with multiple access points and VLAN segmenting.
         Since then I have really been emersed in web development and plan to continue and grow as new technologies arrive!
--- src/assets/components/Portfolio.jsx
+++ src/assets/components/Portfolio.jsx
@@ -1,12 +1,26 @@
 import Header from './Header';
+import React, { useState } from 'react';
 const Portfolio = () => {
+    const [hovered, setHovered] = useState(false);
+
+    const handleEnter = () => {
+      setHovered(true)
+    }
+
+    const handleLeave = () => {
+      setHovered(false)
+    }
+
   return (
     <>
     <Header port="bg-gray-500" />
     <body className="h-screen flex flex-col px-1.5 items-center ">
       <h1 className="text-gray-400 text-bold font-bold text-5xl pt-12">Portfolio</h1>
       <div className="grid grid-cols-3 pt-12 gap-20">
-      <img src="pfp.png" alt="Profile Picture" width="200" height="200"/>
+      <img src="restaurant.jpg" alt="Profile Picture" width="300" height="300" onMouseEnter={handleEnter} onMouseLeave={handleLeave}/>
+      <h3 className="absolute text-4xl font-bold text-white px-1.5">Yum's</h3>
+      {hovered && (<a href='https://github.com/zamorejake' className="absolute pt-28" target="_blank" rel="noreferrer"><img src="github.png" alt="Github logo" width="75" height="75" onMouseEnter={handleEnter} onMouseLeave={handleLeave}/></a>)}
+      {hovered && (<a href='https://yums-4103a315e67a.herokuapp.com/' className="absolute pt-28 pl-20" target="_blank" rel="noreferrer"><img src="deploy.png" alt="Deployment logo" width="75" height="75" onMouseEnter={handleEnter} onMouseLeave={handleLeave}/></a>)}
       <img src="pfp.png" alt="Profile Picture" width="200" height="200"/>
       <img src="pfp.png" alt="Profile Picture" width="200" height="200"/>
       <img src="pfp.png" alt="Profile Picture" width="200" height="200"/>
