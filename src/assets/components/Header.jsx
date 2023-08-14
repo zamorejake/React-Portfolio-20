704a96b7a717ca5cc3acf3813d093ee469e54767
Jake <zamorejake@gmail.com>
2023-08-14 13:02:42 -0500

    added logic for portfolio hover

--- src/assets/components/Header.jsx
+++ src/assets/components/Header.jsx
@@ -5,7 +5,7 @@ const Header = ({ about, port, con, res }) => {
       <h1 className="text-6xl text-white font-bold">Jake Zam</h1>
       </div>
       <div>
-        <ul className="flex space-x-4 font-semibold">
+        <ul className="flex text-white space-x-4 font-semibold">
           <a href="/AboutMe" className={`basis-40 ${about}`}> <li className="hover:font-black">About Me</li></a>
           <a href="/Portfolio" ><li className={`basis-20 hover:font-black ${port}`}>Portfolio</li></a>
           <a href="/Contact" ><li className={`basis-20 hover:font-black ${con}`}>Contact</li></a>
