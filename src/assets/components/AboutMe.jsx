0a665c8420e41befc7ae64667bbd0ead615e1a30
Jake <zamorejake@gmail.com>
2023-10-16 21:11:28 -0500

    Added logos to replace text and made everything mobile friendly

--- src/assets/components/AboutMe.jsx
+++ src/assets/components/AboutMe.jsx
@@ -1,27 +1,36 @@
-import React from 'react';
-import TypeItText from './TypeIt';
+import React from "react";
+import TypeItText from "./TypeIt";
 const AboutMe = ({ onSectionClick }) => {
-  
-    return (
-      <>
+  return (
+    <>
       <div id="AboutMe" className="h-screen flex flex-col px-1.5 items-center">
-        <h1 className="text-black text-bold font-bold text-5xl pt-28 drop-shadow-[0_2px_2px_rgba(255,255,255,0.8)]">About Me</h1>
+        <h1 className="text-black text-bold font-bold text-5xl pt-28 sm:pt-66 lg:pt-28 drop-shadow-[0_2px_2px_rgba(255,255,255,0.8)]">
+          About Me
+        </h1>
+
         <div className="flex-shrink-0 pt-5">
-        <img src="pfp2.png" alt="Profile Picture" width="200" height="200"/>
+          <img src="pfp2.png" alt="Profile Picture" width="200" height="200" />
         </div>
         <a
-              href="#Portfolio"
-              onClick={() => onSectionClick("portfolio")}
-              className="hover:font-black"
-            >
-        <div className="pt-6 items-center px-14 text-3xl text-white drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)]"><TypeItText />
-        </div>
+          href="#Portfolio"
+          onClick={() => onSectionClick("portfolio")}
+          className="hover:font-black"
+        >
+          <div className="pt-6 items-center px-14 text-3xl text-white drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)]">
+            <TypeItText />
+          </div>
         </a>
-        <p className="pt-6 items-center px-14 text-3xl font-bold text-black drop-shadow-[0_1.2px_1.2px_rgba(255,255,255.8)]">My name is Jake Zamarripa and I'm a web developer based in the KC metro area. I'm passionate about coding and work well in a team oriented environments and individually. I am excited to contribute my skills and creativity to drive success in future projects!</p>
-     </div>
-      </>
-    );
-  };
-  
-  export default AboutMe;
-  
+        <div className="flex justify-center h-screen">
+          <p className="text-center text-3xl font-bold text-black px-6 max-w-screen-md mx-auto pt-6 px-14 mb-4 sm:mb-8 lg:mb-12 drop-shadow-[0_1.2px_1.2px_rgba(255,255,255.8)]">
+            My name is Jake Zamarripa and I'm a web developer based in the KC
+            metro area. I'm passionate about coding and work well in a team
+            oriented environments and individually. I am excited to contribute
+            my skills and creativity to drive success in future projects!
+          </p>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default AboutMe;
--- src/assets/components/Resume.jsx
+++ src/assets/components/Resume.jsx
@@ -1,24 +1,83 @@
+import React, { useState } from "react";
 const Resume = () => {
+  const [hovered, setHovered] = useState(false);
+
+  const handleEnter = (index) => {
+    setHovered(index);
+  };
+
+  const handleLeave = () => {
+    setHovered(false);
+  };
+  const logoData = [
+    {
+      image: "icons/html.png",
+      alt: "HTML Logo",
+    },
+    {
+      image: "icons/css.png",
+      alt: "CSS Logo",
+    },
+    {
+      image: "icons/js.png",
+      alt: "JavaScript Logo",
+    },
+    {
+      image: "icons/node.png",
+      alt: "Node.js Logo",
+    },
+    {
+      image: "icons/react.png",
+      alt: "React Logo",
+    },
+    {
+      image: "icons/mysql.png",
+      alt: "MySQL Logo",
+    },
+    {
+      image: "icons/mongo.png",
+      alt: "MongoDB Logo",
+    },
+    {
+      image: "icons/express.png",
+      alt: "Express.js Logo",
+    },
+  ];
   return (
     <>
       <div id="Resume" className="h-screen flex flex-col px-1.5 items-center ">
         <h1 className="text-black text-bold font-bold text-5xl pt-28 drop-shadow-[0_2px_2px_rgba(255,255,255,0.8)]">
           Resume
         </h1>
-        <a href="Resume.pdf" download>
-          <h2 className="pt-12 text-3xl underline">Download my Resume!</h2>
-        </a>
-        <h1 className="pt-16 font-bold text-4xl">Technical Skills</h1>
-        <ul className="pt-7 text-2xl">
-          <li>HTML</li>
-          <li>CSS</li>
-          <li>Javascript</li>
-          <li>MongoDB</li>
-          <li>MySQL</li>
-          <li>Express</li>
-          <li>React</li>
-          <li>Node.js</li>
-        </ul>
+        <iframe
+          className="pt-5"
+          src="Resume.pdf"
+          width="600"
+          height="630"
+        ></iframe>
+        <h1 className="pt-5 font-bold text-4xl">Technical Skills</h1>
+        <div className="grid grid-cols-4 gap-2.5 pb-12">
+          {logoData.map((logoData, index) => (
+            <div
+              key={index}
+              className="relative pt-14"
+              onMouseLeave={handleLeave}
+              onMouseEnter={() => setHovered(index)}
+            >
+              <img
+                src={logoData.image}
+                alt={logoData.alt}
+                width="100"
+                height="100"
+                className={`image-container transform transition-transform ease-in-out duration-250 ${
+                  hovered === index
+                    ? "brightness-50 scale-110"
+                    : "hover:brightness-50 hover:scale-110"
+                }`}
+              />
+            </div>
+          ))}
+        </div>
       </div>
     </>
   );
